6d4d3c31c4221df07bf3039d2d3fb6ec9640c5b5
Oleg Dugaev <dugaevoleg9@gmail.com>
2024-02-07 18:55:10 +0200

    colors back

--- pages/Settings.vue
+++ pages/Settings.vue
@@ -15,11 +15,11 @@ const logout = async () => {
 </script>
 
 <template>
-  <h1 class="p-10 font-bold text-2xl mb-10 text-center">Settings</h1>
+  <h1 class="p-10 font-bold text-2xl mb-10 text-center">Customers</h1>
   <div class="text-center items-center">
     <button
       @click="logout"
-      class="mt-36 mb-[20px] transition-colors hover:text-[#FFA500] border py-3 px-6 rounded-lg"
+      class="mt-36 mb-[20px] transition-colors hover:text-[#8500f6] border py-3 px-6 rounded-lg"
     >
       <Icon name="line-md:log-out" size="26" class="mb-1" />
       Log Out
